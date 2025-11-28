18e017206ef60a6f2d6f4ff2835e5c585f09dd39
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-11-29 04:31:46 +0500

    7

---
+++ Tyuiu.AfoninME.Sprint5.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.AfoninME.Sprint5.Task7.V13.Lib;
+
+namespace Tyuiu.AfoninME.Sprint5.Task7.V13.Test;
+{
+    public class DataServiceTest
+{
+    [Fact]
+    public void CheckLoadFromDataFile()
+    {
+        var result = new DataService();
+        Assert.Equals(true, new FileInfo($@"{Path.GetTempPath()}\OutPutDataFileTask7V13.txt").Exists);
+    }
+}
+}
